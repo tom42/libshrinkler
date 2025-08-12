79609c51af258ad1cdd941f8382fd77149c005b4
tom <tom@z2>
2025-08-12 22:07:51 +0200

    Add test: parity on, big endian

--- test/libshrinkler_unit_test/encoder_test.cpp
+++ test/libshrinkler_unit_test/encoder_test.cpp
@@ -26,12 +26,24 @@ bytevector make_bytevector(const char* s)
 }
 
 // TODO: new tests
-//       * Big endian output
 //       * Empty input (would have been a natural first test...)
 TEST_CASE("encoder_test")
 {
     encoder_parameters parameters;
     encoder encoder;
+
+    SECTION("parity, big endian")
+    {
+        const auto original_data = make_bytevector("foo foo foo foo");
+        const bytevector expected_encoded_data{0x99, 0xc8, 0x62, 0xc6, 0x9b, 0x39, 0x00};
+        parameters.parity_context(true);
+        parameters.endianness(endianness::big);
+        encoder.parameters(parameters);
+
+        auto actual_encoded_data = encoder.encode(original_data);
+
+        CHECK(actual_encoded_data == expected_encoded_data);
+    }
 
     SECTION("parity, little endian")
     {
