4cc91e08906bbcd790f1d3e870a6dbbdd0b15e74
tom <tom@z2>
2025-08-16 09:29:58 +0200

    Sketch out unit test for int_cast

---
+++ test/libshrinkler_unit_test/int_cast_test.cpp
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: 2025 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+import libshrinkler;
+
+namespace libshrinkler_unit_test
+{
+
+TEST_CASE("int_cast_test")
+{
+    // TODO: check 0 => good
+    // TODO: check int_max cast to size_t => good
+    // TODO: check int_max cast to size_t => should throw
+    // TODO: need unit test version before we can do this
+}
+
+}
