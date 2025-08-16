4141fb4b857250fe5ad45f7c2dc43b4850df4144
tom <tom@z2>
2025-08-16 09:56:25 +0200

    Add test for int_cast (not yet complete)

--- test/libshrinkler_unit_test/int_cast_test.cpp
+++ test/libshrinkler_unit_test/int_cast_test.cpp
@@ -2,18 +2,24 @@
 // SPDX-License-Identifier: MIT
 
 #include <catch2/catch_test_macros.hpp>
+#include <cstddef>
+#include <limits>
 
 import libshrinkler;
 
 namespace libshrinkler_unit_test
 {
 
+using libshrinkler::int_cast;
+
 TEST_CASE("int_cast_test")
 {
-    // TODO: check 0 => good
-    // TODO: check int_max cast to size_t => good
-    // TODO: check int_max cast to size_t => should throw
-    // TODO: need unit test version before we can do this
+    // TODO: check int_max cast to size_t => should throw => this can only be done if size_t is wider than int, no?
+    constexpr std::size_t min = 0;
+    constexpr std::size_t max = std::numeric_limits<int>::max();
+
+    CHECK(int_cast(min) == min);
+    CHECK(int_cast(max) == max);
 }
 
 }
