86510bb50ae02f48fa0c64f4d4d957340f2c2b36
tom42 <tom42@users.noreply.github.com>
2025-08-21 00:02:08 +0200

    Adjust exception message

--- src/libshrinkler/encoder.cpp
+++ src/libshrinkler/encoder.cpp
@@ -116,7 +116,7 @@ void verify(byte_vector& compressed_data, byte_vector& uncompressed_data, const 
     decoder.setListener(&verifier);
     if (!lzd.decode(verifier))
     {
-        throw internal_error("verification of compressed data failed");
+        throw internal_error("verify error: verification of compressed data failed");
     }
 
     // TODO: verify (reference code from shrinkler below)
